398cc0ddcbc3213f0c71d41e1020e456e15761eb
Anna Szymula <ania.szymula505@gmail.com>
2020-12-07 11:46:45 -0600

    ethinicity data updated

--- server/db/models/ethnicity.js
+++ server/db/models/ethnicity.js
@@ -9,56 +9,28 @@ const Ethnicity = db.define('ethnicity', {
         type: Sequelize.INTEGER,
         defaultValue: 0,
     },
-    caucasianPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
-        defaultValue: 0,
-    },
     africanAmericanTotals: {
         type: Sequelize.INTEGER,
-        defaultValue: 0,
-    },
-    africanAmericanPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
         defaultValue: 0,
     },
     nativeAmericanTotals: {
         type: Sequelize.INTEGER,
         defaultValue: 0,
     },
-    nativeAmericanPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
-        defaultValue: 0,
-    },
     asianAmericanTotals: {
         type: Sequelize.INTEGER,
-        defaultValue: 0,
-    },
-    asianAmericanPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
         defaultValue: 0,
     },
     pacificIslanderTotals: {
         type: Sequelize.INTEGER,
         defaultValue: 0,
     },
-    pacificIslanderPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
-        defaultValue: 0,
-    },
     latinoAmericanTotals: {
         type: Sequelize.INTEGER,
         defaultValue: 0,
     },
-    latinoAmericanPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
-        defaultValue: 0,
-    },
     otherTotals: {
         type: Sequelize.INTEGER,
-        defaultValue: 0,
-    },
-    otherPercentage: {
-        type: Sequelize.DECIMAL(10, 1),
         defaultValue: 0,
     },
     createdAt: {
