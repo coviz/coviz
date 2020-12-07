ba63190aedf43befbcdc96533492da2280f5ebef
Anna Szymula <ania.szymula505@gmail.com>
2020-12-06 19:15:52 -0600

    minor style changes

--- server/db/db.js
+++ server/db/db.js
@@ -3,19 +3,19 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-//   {
-//     logging: false
-//   }
-// )
-
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://ania:newPassword@localhost/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false
   }
 )
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://ania:newPassword@localhost/${databaseName}`,
+//   {
+//     logging: false
+//   }
+// )
 
 module.exports = db
 
