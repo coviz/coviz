56b91f8c88c275457e7c64b1d964747b32e752b3
Anna Szymula <ania.szymula505@gmail.com>
2020-12-07 12:04:38 -0600

    adjusting for updated ethn data

--- client/store/ethnicity.js
+++ client/store/ethnicity.js
@@ -1,52 +1,5 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
-const SET_ETHNICITIES = 'SET_ETHNICITIES';
-
-export const setEthnicities = usData => ({
-    type: SET_ETHNICITIES,
-    usData
-});
-
-export const fetchEthnicities = () => {
-    return async dispatch => {
-        try {
-            console.log('before axios')
-            const {data} = await axios.get("/api/ethnicity")
-            console.log('after axios')
-            console.log('data in thunk rn:', data)
-            let mappedData = Object.keys(data[0]).map(ethnKey => {
-                // return ethnKey.includes('Totals')
-                if (ethnKey.includes('Totals')) {
-                    // console.log('fshdksd', data[0][ethnKey])
-                    return data[0][ethnKey]
-                    // return true
-                } 
-            }).filter(dataBit => {
-                return dataBit !== undefined
-            })
-
-            console.log('mapped in thunk', mappedData)
-            dispatch(setEthnicities(data))
-        } catch (error) {
-            console.log(error)
-        }
-    }
-}
-
-const initialState = {
-    usData: {}
-}
-
-export default function ethnicityReducer(state=initialState, action) {
-    switch(action.type) {
-        case SET_ETHNICITIES:
-            return {...state, usData: action.usData}
-        default:
-            return state
-    }
-}
-=======
 const SET_ETHNICITIES = 'SET_ETHNICITIES'
 
 export const setEthnicities = usData => ({
@@ -67,9 +20,9 @@ export const fetchEthnicities = () => {
             return data[0][ethnKey]
           }
         })
-        .filter(dataBit => {
-          return dataBit !== undefined
-        })
+        // .filter(dataBit => {
+        //   return dataBit !== undefined
+        // })
 
       console.log('mapped in thunk', mappedData)
       dispatch(setEthnicities(mappedData))
@@ -92,4 +45,3 @@ export default function ethnicityReducer(state = initialState, action) {
       return state
   }
 }
->>>>>>> main
