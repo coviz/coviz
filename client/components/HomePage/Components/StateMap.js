df310b1f854acd2ebd9de57c1f6a00121e3bf098
jin young <jinnyxchoi@gmail.com>
2020-12-10 05:42:11 +0900

    merged with main

--- client/components/HomePage/Components/StateMap.js
+++ client/components/HomePage/Components/StateMap.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import {
   geoAlbersUsa,
   geoPath,
@@ -13,9 +13,11 @@ import d3Tip from 'd3-tip'
 import usData from '../Assets/usData.json'
 
 export const StateMap = data => {
-  // const [geographies, setGeographies] = useState([])
+  const [mode, setMode] = useState(false)
 
   const svgRef = useRef()
+  const maxCases = max(data.data.map(d => d.positive))
+  let radiusScale = scaleSqrt()
 
   useEffect(
     () => {
@@ -59,12 +61,6 @@ export const StateMap = data => {
         .style('stroke', '#080B0C')
         .style('stroke-width', 2)
 
-      const maxCases = max(data.data.map(d => d.positive))
-
-      const radiusScale = scaleSqrt()
-        .domain([0, 1, maxCases])
-        .range([0, 2, 75])
-
       const circleData = svg
         .selectAll('.circle')
         .data(data.data, d => d.statecode)
@@ -90,8 +86,17 @@ export const StateMap = data => {
         .transition()
         .duration(10)
         .ease(easeLinear)
-        // Update the radius to the new cases value
-        .attr('r', d => radiusScale(d.positive))
+      if (mode) {
+        circleData.attr('r', d =>
+          radiusScale.domain([0, 1, d.population / 10]).range([0, 2, 75])(
+            d.positive
+          )
+        )
+      } else {
+        circleData.attr('r', d =>
+          radiusScale.domain([0, 1, maxCases]).range([0, 2, 75])(d.positive)
+        )
+      }
 
       // Exit data points no longer in data and remove
       circleData.exit().remove()
@@ -136,11 +141,21 @@ export const StateMap = data => {
         })
         .on('mouseout', tooltip.hide)
     },
-    [svgRef, data]
+    [svgRef, data, mode]
   )
 
   return (
     <div>
+      <div>
+        <select
+          onChange={() => {
+            setMode(!mode)
+          }}
+        >
+          <option value="maxcases"> new cases/max cases</option>
+          <option value="pop"> new cases/state population</option>
+        </select>
+      </div>
       <svg
         id="mainMap"
         ref={svgRef}
--- client/components/HomePage/Components/Trend.js
+++ client/components/HomePage/Components/Trend.js
@@ -47,7 +47,6 @@ export default ({time, startTime, endTime, updateTime}) => {
     /** Pass the relative click position through the xScale invert function to get the appropriate date */
 
     updateTime(xScale.invert(clickX >= 0 ? clickX : 0))
-    console.log('LOOK', xScale.invert(clickX >= 0 ? clickX : 0))
   }
 
   return (
