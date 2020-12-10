17794da2552a548462fbffe6021041af998051d8
Anna Szymula <ania.szymula505@gmail.com>
2020-12-10 03:57:42 -0600

    facts about age group data added

--- client/components/homePage/age.js
+++ client/components/homePage/age.js
@@ -19,10 +19,23 @@ export const AgeChart = props => {
   console.log('data: ', data)
   return (
     <div>
-      <h2 style={{color: "white"}}>Covid v. Age Groups</h2>
+      <h1 style={{color: "white"}}>Covid v. Age Groups</h1>
       {isLoading ? drawAgeChart(650, 600, data) : <div />}
-      <div style={{margin: 50}}id="ageChart" />
-      <p>HELLLLLOOOOO</p>
+      <div className="chartNinfo">
+        <div style={{margin: 50}}id="ageChart" />
+        <div>
+            <b>The Effect of COVID-19 on Different Age Groups</b>
+            <br></br>
+            <ul className="ageFacts">
+              <li>As clearly as it is displayed in the visual, the risk of dying from Coronavirus increases greatly with age.</li>
+              <li>"COVID-19 is not just hazardous for elderly people, it is extremely dangerous for people in their mid-fifties, sixties and seventies"</li>
+              <li>The death totals for the 85 and older age group are almost double for that of the 50 through 64 year age group.</li>
+              <li>"Children, under the age of 18, to date, account for less than one percent of reported COVID-19 deaths"</li>
+              <li>"The risk of dying from coronavirus is also linked to underlying health conditions, the capacity of health-care systems, and whether the virus has spread among people living in elderly-care facilities."</li>
+            </ul>
+        </div>
+      </div>
+      
     </div>
   )
 }
