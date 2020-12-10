1613b09a83f2ae1e7132833f745a7d75d821d496
jin young <jinnyxchoi@gmail.com>
2020-12-10 10:15:36 +0900

    testing

--- script/seedStateModel.js
+++ script/seedStateModel.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
-const Pool = require('pg').Pool
+const pg = require('pg')
 const fastcsv = require('fast-csv')
 const db = require('../server/db')
+// const connectionString = "postgres://ngzkezcbdbrhah:4f712c9e90a2eeca1c40e8aad69062f2c468475d911c6c4a2611bc62ea006c03@ec2-54-235-116-235.compute-1.amazonaws.com:5432/dddc7ahebtdgje"
 
 async function createStateTable() {
   await db.sync()
@@ -19,21 +20,21 @@ async function createStateTable() {
       csvData.shift()
 
       // create a new connection to the database
-      const pool = new Pool({
-        host: process.env.HOST || 'localhost',
-        user: process.env.USER || 'postgres',
-        // ^^comment this 1 line in when not on Anna's comp^^
-        // user: 'ania',
-        // password: 'newPassword',
-        // ^^comment these 2 lines out when not on Anna's comp^^
-        database: process.env.DATABASE || 'coviz',
-        port: 5432
-      })
+      // const pool = new Pool({
+      //   host: process.env.HOST || 'localhost',
+      //   user: process.env.USER || 'postgres',
+      //   // ^^comment this 1 line in when not on Anna's comp^^
+      //   // user: 'ania',
+      //   // password: 'newPassword',
+      //   // ^^comment these 2 lines out when not on Anna's comp^^
+      //   database: process.env.DATABASE || 'coviz',
+      //   port: 5432
+      // })
 
       const query =
         'INSERT INTO "states" (state, capital, latitude, longitude, population, "statecode") VALUES ($1, $2, $3, $4, $5, $6)'
-
-      pool.connect((err, client, done) => {
+      console.log('process.env', process.env.DATABASE_URL)
+      pg.connect(process.env.DATABASE_URL, (err, client, done) => {
         if (err) throw err
 
         try {
