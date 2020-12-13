499ca909e1f2562ed684db3d1ea1abe89281d753
jin young <jinnyxchoi@gmail.com>
2020-12-13 20:12:21 +0900

    trying to reseed

--- script/seedCovidDailyModel.js
+++ script/seedCovidDailyModel.js
@@ -53,25 +53,54 @@ async function createCovidDailyTable() {
 
       pool.connect((err, client, done) => {
         if (err) throw err
+        let timer = new Date(2020, 1, 22)
+        let lastDate = new Date(2020, 1, 28)
 
-        try {
-          csvData.forEach(row => {
-            client.query(query, row, (err, res) => {
-              if (err) {
-                console.log(err.stack)
-              } else {
-                console.log('inserted ' + res.rowCount + ' row:', row)
+        // let c = '20200222'
+        // let year = c.slice(0, 4)
+        // year = Number(year)
+        // let month = c.slice(4, 6)
+        // month = Number(month) - 1
+        // let day = c.slice(6, 8)
+        // day = Number(day)
+        // let rowDate = new Date(year, month, day)
+        // console.log('rowDate', rowDate)
+        // console.log('timer', timer)
+        // console.log('ROWDATE', rowDate.getTime())
+        // console.log('TIMER', timer.getTime())
+        // console.log(timer.getTime() === rowDate.getTime())
+
+        while (timer <= lastDate) {
+          try {
+            csvData.forEach(row => {
+              let year = row[0].slice(0, 4)
+              year = Number(year)
+              let month = row[0].slice(4, 6)
+              month = Number(month) - 1
+              let day = row[0].slice(6, 8)
+              day = Number(day)
+              let rowDate = new Date(year, month, day)
+
+              if (rowDate.getTime() === timer.getTime()) {
+                client.query(query, row, (err, res) => {
+                  if (err) {
+                    console.log(err.stack)
+                  } else {
+                    console.log('inserted ' + res.rowCount + ' row:', row)
+                  }
+                })
               }
+              timer = new Date(timer.setDate(timer.getDate() + 2))
             })
-          })
-        } catch {
-          // what is the difference between done() and end() and close()
-          // done()
+          } catch (error) {
+            console.log(error)
+          }
         }
+
+        // pool.end(() => {
+        //   console.log('pool has ended')
+        // })
       })
-      // pool.end(() => {
-      //   console.log('pool has ended')
-      // })
     })
 
   stream.pipe(csvStream)
@@ -80,3 +109,29 @@ async function createCovidDailyTable() {
 createCovidDailyTable()
 
 // module.exports = createCovidDailyTable()
+
+// pool.connect((err, client, done) => {
+
+//   let timer = new Date(2020, 0, 22)
+//   csvData.forEach((row) => {
+//     let year = row[0].slice(0,4)
+//     let month= row[0].slice(4,6)
+//     let day= row[0].slice(6,8)
+//     let rowDate = new Date(year, month, day)
+
+//     if (rowDate.getTime() === timer.getTime()) {
+//       client.query(query, row, (err, res) => {
+//         if (err) {
+//           console.log(err.stack)
+//         } else {
+//           console.log('inserted ' + res.rowCount + ' row:', row)
+//         }
+//       })
+//     }
+//     timer.setDate(timer.getDate() + 2);
+
+// })
+// // pool.end(() => {
+// //   console.log('pool has ended')
+// // })
+// })
