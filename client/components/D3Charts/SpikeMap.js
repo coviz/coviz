59c82c466707de2f51f453ae6cef0eee5d723ae8
jin young <jinnyxchoi@gmail.com>
2020-12-15 04:05:32 +0900

    legend and tooltip

--- client/components/D3Charts/SpikeMap.js
+++ client/components/D3Charts/SpikeMap.js
@@ -13,7 +13,6 @@ export const SpikeMap = realData => {
     [0, 200]
   )
 
-  const format = d3.format(',.0f')
   const spike = (val, width = 7) => `M${-width / 2},0L0,${-val}L${width / 2},0`
 
   useEffect(
@@ -28,7 +27,7 @@ export const SpikeMap = realData => {
       var div = d3
         .select('body')
         .append('div')
-        .attr('class', 'tooltip')
+        .attr('class', 'tooltipjails')
         .style('opacity', 0)
 
       // setGeographies(usData.features)
@@ -47,6 +46,34 @@ export const SpikeMap = realData => {
         .style('stroke', '#080B0C')
         .style('stroke-width', 2)
 
+      const legend = svg
+        .append('g')
+        .attr('fill', '#777')
+        .attr('text-anchor', 'middle')
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', 10)
+        .selectAll('g')
+        .data(
+          length
+            .ticks(4)
+            .slice(1)
+            .reverse()
+        )
+        .join('g')
+        .attr('transform', (d, i) => `translate(${975 - (i + 1) * 18},590)`)
+
+      legend
+        .append('path')
+        .attr('fill', 'red')
+        .attr('fill-opacity', 0.3)
+        .attr('stroke', 'red')
+        .attr('d', d => spike(length(d)))
+
+      legend
+        .append('text')
+        .attr('dy', '1.3em')
+        .text(length.tickFormat(4, 's'))
+
       svg
         .append('g')
         .attr('fill', 'red')
@@ -57,6 +84,33 @@ export const SpikeMap = realData => {
         .join('path')
         .attr('transform', d => `translate(${projection([d.long, d.lat])})`)
         .attr('d', d => spike(length(d.confirmedResidents)))
+        .on('mouseover', function(d) {
+          const toolData = d.srcElement.__data__
+          div
+            .transition()
+            .duration(200)
+            .style('opacity', 0.9)
+          div
+            .html(
+              '<b><u>' +
+                ` ${toolData.state}` +
+                '</u></b>' +
+                '<br>' +
+                `facility name: ${toolData.name}` +
+                '<br>' +
+                `total resident cases: ${toolData.confirmedResidents}`
+            )
+            .style('left', d.pageX + 'px')
+            .style('top', d.pageY - 28 + 'px')
+        })
+
+        // fade out tooltip on mouse out
+        .on('mouseout', function(d) {
+          div
+            .transition()
+            .duration(500)
+            .style('opacity', 0)
+        })
     },
     [svgRef, data]
   )
