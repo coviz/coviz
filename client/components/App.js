a9f6316ef0b7530366511a1b4eec3d368a8d9ff9
jaznyc <jaznycdev@gmail.com>
2021-01-22 12:51:04 -0500

    fixed timer bug from data update

--- client/components/App.js
+++ client/components/App.js
@@ -15,7 +15,7 @@ export const App = () => {
   // create the timer
   const timer = useTimer({
     startTime: new Date('2020-02-28'),
-    endTime: new Date('2021-01-20'),
+    endTime: new Date('2021-01-19'),
     step: 1000 * 60 * 60 * 72,
     frequency: 2
   })
@@ -85,7 +85,8 @@ export const App = () => {
           <div>
             {timerDate === '1/19/2021' ||
             timerDate === '1/20/2021' ||
-            timerDate === '1/21/2021' ? (
+            timerDate === '1/21/2021' ||
+            timerDate === '1/22/2021' ? (
               <button
                 type="button"
                 className="play-button"
