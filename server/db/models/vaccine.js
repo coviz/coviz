630609262153442ebf280c43ed7a3a93db8f2ff6
jaznyc <jaznycdev@gmail.com>
2021-03-25 17:19:51 -0400

    model for vaccine comp completed, dataset for vaccine added

--- server/db/models/vaccine.js
+++ server/db/models/vaccine.js
@@ -1,2 +1,27 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+
+const Vaccine = db.define('vaccine', {
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  date: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  vaccinations: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    allowNull: true
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: true
+  }
+})
+
+module.exports = Vaccine
