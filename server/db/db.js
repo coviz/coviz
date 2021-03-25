96df361bfb630a552a5255422f4f23e3ef87bcee
jaznyc <jaznycdev@gmail.com>
2021-03-25 16:58:56 -0400

    logic for heroku ssl switch

--- server/db/db.js
+++ server/db/db.js
@@ -3,14 +3,28 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    dialect: 'postgres',
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
     dialectOptions: {
-      ssl: true
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
+} else {
+  config = {
+    logging: false
+  }
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
 )
 
 // const db = new Sequelize(
