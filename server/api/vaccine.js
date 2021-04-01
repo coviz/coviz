fba2bc921976c963b664ebb27c58d90bcd85d294
jaznyc <jaznycdev@gmail.com>
2021-04-01 18:04:19 -0400

    created get route for vaccines

--- server/api/vaccine.js
+++ server/api/vaccine.js
@@ -2,3 +2,13 @@ const router = require('express').Router()
 const Sequelize = require('sequelize')
 const {Vaccine} = require('../db/models')
 module.exports = router
+
+//GET route for all Vaccine data
+router.get('/', async (req, res, next) => {
+  try {
+    const vaccine = await Vaccine.findAll()
+    res.json(vaccine)
+  } catch (err) {
+    next(err)
+  }
+})
